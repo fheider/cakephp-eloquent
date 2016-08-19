15b8bbe03ce6b6277f4f07ad3bc561ec980e496d
Frank Heider <heider@brainbo.net>
2016-08-19 22:15:34 +0200

    Project start

---
+++ src/ORM/Entity.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CakeORM\ORM;
+
+
+use Cake\ORM\Entity as BaseEntity;
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Inflector;
+
+
+class Entity extends BaseEntity
+{
+
+    public $_table = null;
+
+
+    public function __construct(array $properties = [], array $options = [])
+    {
+        parent::__construct($properties, $options);
+        return $this;
+    }
+
+    public function save()
+    {
+        $table = $this->_getTable();
+        return (new $table)->instance()->save($this);
+    }
+
+    public function delete()
+    {
+        $table = $this->_getTable();
+        return (new $table)->instance()->delete($this);
+    }
+
+    private function _getTable()
+    {
+        if ($this->_table === null) {
+            $this->_table = str_replace('Model\Entity', 'Model\Table', Inflector::pluralize(static::class)) . 'Table';
+        }
+        return $this->_table;
+    }
+
+    public function source($alias = null)
+    {
+        $table = $this->_getTable();
+        return (new $table)->instance();
+    }
+
+}
---
+++ src/ORM/Table.php
@@ -0,0 +1,108 @@
+<?php
+namespace CakeORM\ORM;
+
+
+use Cake\Datasource\ConnectionManager;
+use Cake\ORM\Table;
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Inflector;
+
+
+class Table
+{
+
+    protected $_instance;
+
+    protected $_entity;
+
+    public function __construct(array $config = [])
+    {
+        $config += [
+            'primaryKey' => 'id',
+            'displayField' => 'name',
+            'connection' => ConnectionManager::get('default'),
+            'alias' => $this->_getAlias(),
+            'entityClass' => $this->_getEntityClass()
+        ];
+        $this->_instance = new Table($config);
+        $this->_instance->primaryKey($config['primaryKey']);
+        $this->_instance->displayField($config['displayField']);
+    }
+
+    /**
+     * Create new
+     *
+     * @param array $data
+     * @return mixed
+     */
+    public static function create($data = [])
+    {
+        $in = new static;
+        $entity = $in->instance()->newEntity($data);
+        return $in->instance()->save($entity);
+    }
+
+    public static function all($options = [])
+    {
+        $in = new static;
+        return $in->instance()->find('all', $options);
+    }
+
+    public static function findList($options = [])
+    {
+        $in = new static;
+        return $in->instance()->find('list', $options);
+    }
+
+    public static function first($options = [])
+    {
+        return static::all($options)->first();
+    }
+
+    public static function get($primaryKey, $options = [])
+    {
+        $in = new static;
+        return $in->instance()->get($primaryKey, $options);
+    }
+
+    public function instance()
+    {
+        return $this->_instance;
+    }
+
+    private function _getAlias()
+    {
+        $alias = str_replace('Table', '', $this->_getClassName());
+        if ($plugin = $this->_getPlugin()) {
+            return $plugin . '.' . $alias;
+        }
+        return $alias;
+    }
+
+    private function _getEntityClass()
+    {
+        $entityClass = str_replace(
+            ['Model\Table', $this->_getClassName()],
+            ['Model\Entity', Inflector::singularize($this->_getAlias())],
+            static::class
+        );
+
+        if (class_exists($entityClass)) {
+            return $entityClass;
+        }
+        return '\App\Model\Entity';
+    }
+
+    private function _getPlugin()
+    {
+        $class = explode('\\', static::class);
+        return ($class[0] == 'App') ? null : $class[0];
+    }
+
+    private function _getClassName()
+    {
+        $class = explode('\\', static::class);
+        return array_pop($class);
+    }
+
+}
